3a29956b6d8c0abfe33fa23eeabf8505ec6fdfc9
ammar180 <ammaromran.work@gmail.com>
2024-10-31 08:58:26 +0300

    signup presentation

--- lib/features/authentication/models/signup_model.dart
+++ lib/features/authentication/models/signup_model.dart
@@ -1,13 +1,51 @@
-import 'package:rentro_car/features/authentication/screens/signup_page_widget.dart';
+import '../screens/signup_page_widget.dart';
 import 'package:rentro_car/utils/customs/app_model.dart';
 import 'package:flutter/material.dart';
-// import 'package:google_fonts/google_fonts.dart';
-// import 'package:provider/provider.dart';
 
-class SignUpModel extends AppModel<SignUpPageWidget> {
-  @override
-  void initState(BuildContext context) {}
+class SignUpPageModel extends AppModel<SignUpPageWidget> {
+  ///  State fields for stateful widgets in this page.
+
+  // State field(s) for signup_username_field widget.
+  FocusNode? signupUsernameFieldFocusNode;
+  TextEditingController? signupUsernameFieldTextController;
+  String? Function(BuildContext, String?)?
+      signupUsernameFieldTextControllerValidator;
+  // State field(s) for signup_email_field widget.
+  FocusNode? signupEmailFieldFocusNode;
+  TextEditingController? signupEmailFieldTextController;
+  String? Function(BuildContext, String?)?
+      signupEmailFieldTextControllerValidator;
+  // State field(s) for signup_passwrod_field widget.
+  FocusNode? signupPasswrodFieldFocusNode;
+  TextEditingController? signupPasswrodFieldTextController;
+  late bool signupPasswrodFieldVisibility;
+  String? Function(BuildContext, String?)?
+      signupPasswrodFieldTextControllerValidator;
+  // State field(s) for signup_confirm_passwrod_field widget.
+  FocusNode? signupConfirmPasswrodFieldFocusNode;
+  TextEditingController? signupConfirmPasswrodFieldTextController;
+  late bool signupConfirmPasswrodFieldVisibility;
+  String? Function(BuildContext, String?)?
+      signupConfirmPasswrodFieldTextControllerValidator;
 
   @override
-  void dispose() {}
+  void initState(BuildContext context) {
+    signupPasswrodFieldVisibility = false;
+    signupConfirmPasswrodFieldVisibility = false;
+  }
+
+  @override
+  void dispose() {
+    signupUsernameFieldFocusNode?.dispose();
+    signupUsernameFieldTextController?.dispose();
+
+    signupEmailFieldFocusNode?.dispose();
+    signupEmailFieldTextController?.dispose();
+
+    signupPasswrodFieldFocusNode?.dispose();
+    signupPasswrodFieldTextController?.dispose();
+
+    signupConfirmPasswrodFieldFocusNode?.dispose();
+    signupConfirmPasswrodFieldTextController?.dispose();
+  }
 }
